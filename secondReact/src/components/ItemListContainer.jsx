6774a7a42138d43209e77c12ddb3c4be29409838
Claudia Salcedo <csprojectsinfo03@gmail.com>
2024-07-30 17:32:59 -0500

    Probando gitignore

--- secondReact/src/components/ItemListContainer.jsx
+++ secondReact/src/components/ItemListContainer.jsx
@@ -1,41 +1,52 @@
 import { useEffect, useState } from "react";
-import jewelery from "./jewelery.json"
 import Counter from "./Counter"
 import { useParams } from "react-router-dom";
-
+import { collection, getDocs, getFirestore } from "firebase/firestore"
 
 const ItemListContainer = () => {
 
-    const [cards, setCards] = useState(jewelery)
+    const [cards, setCards] = useState([])
     const { id } = useParams();
 
-    useEffect(() => {
-        setCards(id ? jewelery.filter (cards => cards.category == id) : jewelery)
+    //useEffect(() => {
+    //  setCards(id ? jewelery.filter (cards => cards.category == id) : jewelery)
 
-    }, [id])
+    // }, [id])
+
+    useEffect(() => {
+        const db = getFirestore();
+        const database = collection(db, "inventory");
+        getDocs(database).then(inventoryCollection => {
+            if(inventoryCollection.docs.length > 0){
+                setCards(inventoryCollection.docs.map(products => ({id:products.id, ...products.data()})));
+                console.log(cards)
+            }else{
+                console.error("Error");
+            }
+            
+        })
+    }, [])
 
     return (
         <>
             <div className="container">
                 <div className="row">
-                    {cards.map(cards => (
-                        <div key={cards.id} className="col">
-                            <div className="card">
-                                <div className="deal-container shadow p-3">
-                                    <img className="card-img-top" src={cards.image} alt="" />
-                                </div>
-                                <div className="card-body">
-                                    <h5 className="card-title text-light">{cards.name}</h5>
-                                    <p className="card-title text-light">{cards.description}</p>
-                                    <p className="card-text text-light">${cards.price}</p>
+                    {cards.map(card => (
+                       <div key={card.id} className="col">
+                       <div className="card">
+                           <div className="deal-container shadow p-3">
+                               <img className="card-img-top" src={card.image} alt="" />
+                           </div>
+                           <div className="card-body">
+                               <h5 className="card-title text-light">{card.name}</h5>
+                               <p className="card-title text-light">{card.description}</p>
+                               <p className="card-text text-light">${card.price}</p>
 
-                                </div>
-                                <Counter products={20}/>
-                            </div>
-                        </div>
+                           </div>
+                           <Counter products={20} />
+                       </div>
+                   </div> 
                     ))}
-
-
                 </div>
             </div>
         </>
--- secondReact/src/components/MainBanner.jsx
+++ secondReact/src/components/MainBanner.jsx
@@ -1,11 +1,21 @@
 const MainBanner = ({ summerdeal }) => {
     return (
-        <section className="wrapper-deal">
-        <div className="container-img">
-            <h1 className="title-deal">{summerdeal}</h1>
-        </div>
-        </section>
-        )
-        }
-    
-        export default MainBanner;
+        <>
+            <section className="wrapper-deal">
+                <div className="container-img">
+                    <h1 className="title-deal">{summerdeal}</h1>
+                </div>
+            </section>
+
+            <section className="container-fluid mt-5 py-3 bg-black">
+                <div className="row">
+                    <div className="col">
+                    <h3 className="text-light">No te pierdas nuestra colección</h3>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default MainBanner;
